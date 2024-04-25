ea037b730f211de44e97dd55fd45fef6b3bc45c7
dineth panditha <92966321+dinethh@users.noreply.github.com>
2024-04-25 14:49:09 +0530

    transaction search APIs are done

---
+++ app/routes/transaction_route.py
@@ -0,0 +1,91 @@
+from flask import jsonify, request
+
+from app import app
+from app.models.transaction_model import Transaction
+
+
+@app.route('/search_transaction', methods=['GET'])
+def search_transaction_by_id():
+    try:
+
+        transaction_id = int(request.form.get('TransactionID'))
+
+        transaction = Transaction.query.get(transaction_id)
+
+        if not transaction:
+            return jsonify({'message': 'Transaction not found', 'code': 'TRANSACTION_NOT_FOUND'}), 404
+
+        transaction_data = {
+            'TransactionID': transaction.transactionID,
+            'Username': transaction.username,
+            'Amount': transaction.amount,
+            'DateTime': transaction.dateTime.strftime('%Y-%m-%d %H:%M:%S'),
+            'TransactionType': transaction.transactionType,
+            'Status': transaction.status,
+            'UserID': transaction.userID
+        }
+
+        return jsonify(transaction_data), 200
+    except ValueError:
+        return jsonify({'message': 'Invalid TransactionID format', 'code': 'INVALID_TRANSACTION_ID'}), 400
+    except Exception as e:
+        return jsonify({'message': str(e), 'code': 'SERVER_ERROR'}), 500
+
+
+@app.route('/transaction_by_user_id', methods=['GET'])
+def search_transaction_by_user_id():
+    try:
+
+        user_id = int(request.form.get('UserID'))
+
+        transactions = Transaction.query.filter_by(userID=user_id).all()
+
+        if not transactions:
+            return jsonify({'message': 'No transactions found for the user', 'code': 'NO_TRANSACTIONS_FOUND'}), 404
+
+        transaction_data = []
+        for transaction in transactions:
+            transaction_info = {
+                'TransactionID': transaction.transactionID,
+                'Username': transaction.username,
+                'Amount': transaction.amount,
+                'DateTime': transaction.dateTime.strftime('%Y-%m-%d %H:%M:%S'),
+                'TransactionType': transaction.transactionType,
+                'Status': transaction.status,
+                'UserID': transaction.userID
+            }
+            transaction_data.append(transaction_info)
+
+        return jsonify(transaction_data), 200
+    except Exception as e:
+        return jsonify({'message': str(e), 'code': 'SERVER_ERROR'}), 500
+
+
+@app.route('/transaction_by_status', methods=['GET'])
+def search_transaction_by_status():
+    try:
+
+        status = request.form.get('status')
+
+        transactions = Transaction.query.filter_by(status=status).all()
+
+        if not transactions:
+            return jsonify(
+                {'message': 'No transactions found with the given status', 'code': 'NO_TRANSACTIONS_FOUND'}), 404
+
+        transaction_data = []
+        for transaction in transactions:
+            transaction_info = {
+                'TransactionID': transaction.transactionID,
+                'Username': transaction.username,
+                'Amount': transaction.amount,
+                'DateTime': transaction.dateTime.strftime('%Y-%m-%d %H:%M:%S'),
+                'TransactionType': transaction.transactionType,
+                'Status': transaction.status,
+                'UserID': transaction.userID
+            }
+            transaction_data.append(transaction_info)
+
+        return jsonify(transaction_data), 200
+    except Exception as e:
+        return jsonify({'message': str(e), 'code': 'SERVER_ERROR'}), 500
