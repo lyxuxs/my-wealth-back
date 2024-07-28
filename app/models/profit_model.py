7bcadf033d222df9b36bbd2f1b20d733bd84b310
linifernando <linifernando123@gmail.com>
2024-07-28 22:29:49 +0530

    add profit & get all profit

--- app/models/profit_model.py
+++ app/models/profit_model.py
@@ -3,7 +3,10 @@ from app import db
 
 class Profit(db.Model):
     profitID = db.Column(db.Integer, primary_key=True)
-    profitAmount = db.Column(db.Float, nullable=False)
+    shareAmount = db.Column(db.JSON, nullable=False)
+    profitType = db.Column(db.String(100), nullable=False)
+    shareType = db.Column(db.String(100), nullable=False)
     dateTime = db.Column(db.DateTime, nullable=False)
     tradeID = db.Column(db.Integer, db.ForeignKey('trade.tradeID'), nullable=False)
     trade = db.relationship('Trade', backref=db.backref('profits', lazy=True))
+    
--- app/routes/profit_route.py
+++ app/routes/profit_route.py
@@ -5,34 +5,45 @@ from flask import request, jsonify
 from app import app, db
 from app.models.profit_model import Profit
 from app.models.trade_model import Trade
+from app.models.user_model import User
 
 
 @app.route('/add_profit', methods=['POST'])
 def add_profit():
     try:
-
         trade_id = int(request.form.get('TradeID'))
-        profit_amount = float(request.form.get('ProfitAmount'))
+        share_amount=request.form.get('shareAmount')
+        profit_type=request.form.get('profitType')
+        share_type=request.form.get('shareType')
 
         trade = Trade.query.get(trade_id)
         if not trade:
             return jsonify({'message': 'Trade not found', 'code': 'TRADE_NOT_FOUND'}), 404
 
-        trade.trade_on_off = False
-        db.session.commit()
-
         new_profit = Profit(
             tradeID=trade_id,
-            profitAmount=profit_amount,
+            shareAmount=share_amount,
+            profitType=profit_type,
+            shareType=share_type,
             dateTime=datetime.utcnow())
 
         db.session.add(new_profit)
         db.session.commit()
+        
+        trade.trade_on_off = False
+        db.session.commit()
+        
+        rt_users = User.query.filter_by(RT=True).all()
+        for user in rt_users:
+            user.RT=False
+            db.session.commit()
 
         response_data = {
             'ProfitID': new_profit.profitID,
             'TradeID': new_profit.tradeID,
-            'ProfitAmount': new_profit.profitAmount,
+            'share_amount':new_profit.shareAmount,
+            'profit_type':new_profit.profitType,
+            'share_type':new_profit.shareType,
             'DateTime': new_profit.dateTime.strftime('%Y-%m-%d %H:%M:%S'),
             'message': 'Profit added successfully',
             'code': 200
@@ -41,3 +52,22 @@ def add_profit():
         return jsonify(response_data), 201
     except Exception as e:
         return jsonify({'message': str(e), 'code': 'SERVER_ERROR'}), 500
+
+@app.route('/getAll_profit', methods=['GET'])
+def getAll_profit():
+    profits = Profit.query.all()
+
+    response_data = []
+    for profit in profits:
+        profit_data = {
+            'ProfitID': profit.profitID,
+            'TradeID': profit.tradeID,
+            'share_amount':profit.shareAmount,
+            'profit_type':profit.profitType,
+            'share_type':profit.shareType,
+            'DateTime': profit.dateTime,
+        }
+        response_data.append(profit_data)
+
+    return jsonify(response_data)
+  
