b9184dac33615efb173d794189ca36419813898d
linifernando <linifernando123@gmail.com>
2024-08-09 23:46:12 +0530

    Create user profit route

---
+++ app/routes/userProfit_route.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from flask import json, request, jsonify
+from app import app, db
+from app.models.userProfit_model import UserProfit
+from app.models.user_model import User
+
+@app.route('/getUserProfit', methods=['GET'])
+def getUserProfit():
+    userID = request.args.get('userID')
+    userProfits = UserProfit.query.filter(UserProfit.userID == userID).all()
+
+    response_data = []
+    for userProfit in userProfits:
+        UserProfit_data = {
+            'userProfitID': userProfit.userProfitID,
+            'dateTime': userProfit.dateTime,
+            'profitType': userProfit.profitType,
+            'profitAmount': userProfit.profitAmount,
+            'profitID': userProfit.profitID,
+            'userID': userProfit.userID,
+        }
+        response_data.append(UserProfit_data)
+
+    return jsonify(response_data)
+
+@app.route('/getAllUserProfit', methods=['GET'])
+def getAll_profit():
+    userProfits = UserProfit.query.all()
+
+    response_data = []
+    for userProfit in userProfits:
+        UserProfit_data = {
+            'userProfitID': userProfit.userProfitID,
+            'dateTime': userProfit.dateTime,
+            'profitType': userProfit.profitType,
+            'profitAmount': userProfit.profitAmount,
+            'profitID': userProfit.profitID,
+            'userID': userProfit.userID,
+        }
+        response_data.append(UserProfit_data)
+
+    return jsonify(response_data)
