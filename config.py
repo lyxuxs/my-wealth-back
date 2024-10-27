e045ba712d81406de7fdc2f85251df71a842b071
LiniFernando <linifernando123@gmail.com>
2024-10-27 19:57:12 +0530

    change OTP send mail config

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
 
-    MAIL_SERVER = 'mail.privateemail.com'
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
-    MAIL_USERNAME = 'noreply@mywealth.com.co'
-    MAIL_PASSWORD = 'jyDbyt-2zavxy-zegrur'
-    MAIL_DEFAULT_SENDER = 'noreply@mywealth.com.co'
+    MAIL_USERNAME = 'investment.mywealth@gmail.com'
+    MAIL_PASSWORD = 'kwns-vldq-aeza-pafa'
+    MAIL_DEFAULT_SENDER = 'investment.mywealth@gmail.com'
